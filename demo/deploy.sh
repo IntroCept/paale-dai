b182c7220220f4f699963cf0f9827ae84940cc27
Ujjwal Ojha <bring2uo@gmail.com>
2017-02-11 14:35:38 +0545

    testing

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-heroku login
 heroku plugins:install heroku-container-registry
-docker build -t registry.heroku.com/paale-dai/web -f demo/Dockerfile
-heroku container:login
+docker build -t registry.heroku.com/paale-dai/web -f demo/Dockerfile .
+/usr/local/heroku/bin/heroku container:login
 docker push registry.heroku.com/paale-dai/web
